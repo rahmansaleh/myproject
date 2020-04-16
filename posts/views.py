8596ed988b123d399e691f8fb81c393b4e6895eb
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-04-17 04:05:27 +0700

    Update views.py
    create post_list, post_detail

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    postlist = Post.objects.filter(draft=False)
+    return render(request, 'home.html', {'postlist': postlist})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, draft=False)
+    return render(request, 'detail.html', {'post': post})
