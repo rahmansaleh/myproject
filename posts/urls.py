673d53a57b0c9e2ba7c85a8c7b6da38609485488
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-04-17 04:13:58 +0700

    Create urls.py

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .views import(
+    post_list,
+    post_detail
+)
+
+urlpatterns = [
+    url(r'^$', post_list, name='post_list'),
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+]
