36310d9b4f6f1c052fca184bdb3da3bfb0f299a9
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-04-16 22:42:15 +0700

    Update admin.py
    adding PostAdmin

--- posts/admin.py
+++ posts/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 
 from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'draft', 'image', 'content', 'timestamp', 'updated']
+    list_display_links = ['title']
+    list_filter = ['draft', 'updated', 'timestamp']
+    list_editable = []
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Post, PostAdmin)
