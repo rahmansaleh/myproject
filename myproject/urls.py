8863ed6a11e2a270670bf7056de3b9e766270e7f
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-04-17 04:14:24 +0700

    Update urls.py
    import include, url, admin
    add url Post

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls import include, url
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include('posts.urls', namespace='posts')),
 ]
 
 if settings.DEBUG:
