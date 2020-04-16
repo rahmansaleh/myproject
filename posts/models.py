a673500d3d9a8e21ec26997ad8a80e96a1c532f8
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-04-16 22:40:50 +0700

    Update models.py
    import datetime
    remove duplicate import

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.db import models
+import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=200),
