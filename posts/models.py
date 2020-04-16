df2c296bdec0b34ed632e2f1ab0de1ef66e56f29
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-04-16 21:31:45 +0700

    Update models.py
    adding Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200),
+    slug = models.SlugField(unique=True),
+    image = models.ImageField(upload_to='post/%Y/%m', blank=True),
+    content = models.TextField(),
+    draft = models.BooleanField(default=False),
+    publish = models.DateField(auto_now=False, auto_now_add=False),
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False),
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True),
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
